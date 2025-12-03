228d249b21326d8c3bc085c117f14681a61939f8
Rob Gibbens <RobGibbens@gmail.com>
2025-12-03 09:33:36 -0500

    Added Api and Web

---
+++ src/CardGames.Poker.Web/Data/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace CardGames.Poker.Web.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
+
